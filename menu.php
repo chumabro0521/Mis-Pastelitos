8c7e9da77732b437f15ab2d701ffedd618f417a5
chumabro0521 <chumabro0521@gmail.com>
2026-07-10 14:19:09 -0500

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El Libro del Sabor</title>
+    <link rel="stylesheet" href=".css/styles.css">
+    <icon rel="icon" href="Imagenes/logo.jpeg">
+</head>
+<body>
+<div class="navbar">
+    <img src="Imagenes/Logito.png" alt="Nuestro Logo" width="120" height="110">    <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="recetas.php" target="_blank">Recetas</a></li>
+            <ul>
+                <li><a href="desayunos.php">Desayunos</a></li>
+                <li><a href="almuerzos.php">Almuerzos</a></li>
+                <li><a href="cenas.php">Cenas</a></li>
+                <li><a href="postres.php">Postres</a></li>
+            </ul>
+        <li><a href="contacto.php">Contacto</a></li>
+        <li><a href="acerca_de.php">Acerca de</a></li>
+        <li><a href="perfil.php">Mi Perfil</a></li>
+        
+    </ul>
+</div>
+</body>
+</html>
